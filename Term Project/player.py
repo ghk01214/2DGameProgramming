60c6f3aa01c4279f11c59f79d3afd18d03b2f8a1
ghk01214 <ghk01214@naver.com>
2020-11-22 05:43:05 +0900

    플레이어 클래스화

--- Term Project/player.py
+++ Term Project/player.py
@@ -1,8 +1,7 @@
 from pico2d import *
 from game_object import *
-import game_framework_image
-
-RES_ANIMATION = 'res/bitmap/sprite/'
+import gfw
+from bullet import Bullet
 
 class Player:
 	KEY_MAP = {
@@ -18,64 +17,77 @@ class Player:
 
 	KEYDOWN_Z = (SDL_KEYDOWN, SDLK_z)
 
-	image = None
-
 	def __init__(self):
-		if self.image == None:
-			self.image = gfwi.load(RES_ANIMATION + 'Idle.png')
-
+		self.image = gfw.image.load(resBM('idle.png'))
 		self.pos = get_canvas_width() // 2, get_canvas_height() // 2
 		self.delta = 0, 0
+		self.target = None
+		self.speed = 200
+		self.time = 0
 		self.frame = 0
 		self.action = 0
+		self.mag = 1
 
-	def draw(self):
-		sx = self.frame * 28
-		sy = self.action * 25
-		self.image.clip_draw(sx, sy, 28, 25, *self.pos)
-
-	def fire(self):
-		bullet = Bullet(self.pos)
-		Bullet.bullets.append(bullet)
-		print("bullet count = %d" % len(Bullet.bullets))
-
-	def handle_event(self, e):
-		pair = (e.type, e.key)
-
-		if pair[0] == SDL_KEYDOWN:
-			if pair[1] == SDLK_UP or pair[1] == SDLK_DOWN or pair[1] == SDLK_LEFT or pair[1] == SDLK_RIGHT:
-				self.image = gfwi.load(RES_ANIMATION + 'Run.png')
-		else:
-			self.image = gfwi.load(RES_ANIMATION + 'Idle.png')
-
-		if pair in Player.KEY_MAP:
-			self.update_delta(*Player.KEY_MAP[pair])
-		elif pair == Player.KEYDOWN_Z:
-			self.fire()
-
-	def player_delta(self):
-		dxs = [-3, 3, -1, 1]
-		mag = dxs[self.action]
-		dx, dy = self.delta
-		return mag+dx, 2 + dy
+		global center
+		center = self.pos
 
 	def update(self):
 		x, y = self.pos
 		dx, dy = self.delta
-		self.pos = x + dx, y + dy
-		self.frame = (self.frame + 1) % 4
-		gravity = 0.05
-		self.delta = dx, dy - gravity
 
-	def update_delta(self, ddx, ddy):
-		dx, dy = self.delta
-		dx += ddx
-		dy += ddy
+		x += dx * self.speed * self.mag * gfw.delta_time
+		y += dy * self.speed * self.mag * gfw.delta_time
 
-		if ddx != 0:
+		px, py = x, y
+		bg_l, bg_b, bg_r, bg_t = self.background.get_boundary()
+		x = clamp(bg_l, x, bg_r)
+		y = clamp(bg_b, y, bg_t)
+
+		done = False
+		
+		self.pos = x, y
+		self.time += gfw.delta_time
+		frame = self.time * 15
+		self.frame = int(frame) % 4
+	#	gravity = 0.05
+	#	delta = dx, dy - gravity
+
+	def draw(self):
+		width, height = 29, 25
+		sx = self.frame * width
+		sy = self.action * height
+		pos = self.background.to_screen(self.pos)
+			
+		self.image.clip_draw(sx, sy, 28, 25, *pos)
+
+	def handle_event(self, e):
+		pair = (e.type, e.key)
+
+		if pair in Player.KEY_MAP:
+			if self.target is not None:
+				self.target = None
+				self.delta = 0, 0
+
+			pdx = self.delta[0]
+			self.delta = point_add(self.delta, Player.KEY_MAP[pair])
+			dx = self.delta[0]
 			self.action = \
-				0 if dx > 0 else \
-				1 if dx < 0 else \
-				1 if ddx > 0 else 0
+				1 if dx < 0 else 0
 
-		self.delta = dx, dy
+			if pair[0] == SDL_KEYDOWN:
+				change_image = True
+			else:
+				change_image = False;
+		elif pair == Player.KEYDOWN_Z:
+			fire()
+
+	def player_delta(self):
+		dxs = [-3, 3, -1, 1]
+		mag = dxs[action]
+		dx, dy = delta
+		return mag + dx, 2 + dy
+
+	def fire(self):
+		bullet = Bullet(pos)
+		Bullet.bullets.append(bullet)
+		print("bullet count = %d" % len(Bullet.bullets))
