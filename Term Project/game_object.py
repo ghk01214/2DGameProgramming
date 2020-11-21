dd31c75c24eb45229bd8159a302f0c42f0b7106f
ghk01214 <ghk01214@naver.com>
2020-11-22 05:41:55 +0900

    game_object 파일 수정

--- Term Project/game_object.py
+++ Term Project/game_object.py
@@ -1,27 +1,78 @@
 from pico2d import *
-import game_framework_image as gfwi
+import gfw
+import random
 
-RES_OBJECT = 'res/bitmap/object/'
+RES_BM = 'res/bitmap/'
+RES_SE = 'res/sound/'
 
-class Bullet:
-	bullets = []
-	image = None
+def resBM(file):
+	return RES_BM + file
 
-	def __init__(self, pos):
-		if Bullet.image == None:
-			self.image = gfwi.load(RES_OBJECT + 'Bullet.png')
-		self.x, self.y = pos[0], pos[1]
-		self.delta = 8
+def resSE(file):
+	return RES_SE + file
 
-	def draw(self):
-		self.image.draw(self.x, self.y)
+def rand(val):
+    return val * random.uniform(0.9, 1.1)
 
-	def update(self):
-		self.x += self.delta
+def point_add(point1, point2):
+    x1,y1 = point1
+    x2,y2 = point2
+    return x1+x2, y1+y2
 
-		if self.x < -10 or self.x > get_canvas_width() + 10:
-			Bullet.bullets.remove(self)
-			print('bullet count = %d' % len(Bullet.bullets))
+def move_obj(obj):
+    obj.pos = point_add(obj.pos, obj.delta)
 
-def res(self):
-	return RES + file
+def collides_box(a, b):
+	(la, ba, ra, ta) = a.get_bb()
+	(lb, bb, rb, tb) = b.get_bb()
+
+	if la > rb: return False
+	if ra < lb: return False
+	if ba > tb: return False
+	if ta < bb: return False
+
+	return True
+
+def distance_sq(point1, point2):
+    x1,y1 = point1
+    x2,y2 = point2
+    return (x1-x2)**2 + (y1-y2)**2
+
+def distance(point1, point2):
+	math.sqrt(distance_sq(point1, point2))
+	
+def draw_collision_box():
+	for obj in gfw.world.all_objects():
+		if hasattr(obj, 'get_bb'):
+			draw_rectangle(*obj.get_bb())
+
+def mouse_xy(event):
+    return event.x, get_canvas_height() - event.y - 1
+
+def pt_in_rect(point, rect):
+    (x, y) = point
+    (l, b, r, t) = rect
+
+    if x < l: return False
+    if x > r: return False
+    if y < b: return False
+    if y > t: return False
+
+    return True
+
+class ImageObject:
+    def __init__(self, imageName, pos):
+        self.imageName = imageName
+        self.image = gfw.image.load(res(imageName))
+        self.pos = pos
+    def draw(self):
+        self.image.draw(*self.pos)
+    def update(self):
+        pass
+    def __getstate__(self):
+        dict = self.__dict__.copy()
+        del dict['image']
+        return dict
+    def __setstate__(self, dict):
+        self.__dict__.update(dict)
+        self.image = gfw.image.load(res(self.imageName))
