4967f04f8ea74bf65997a4122453f9194565d6d8
ghk01214 <ghk01214@naver.com>
2020-11-23 03:48:55 +0900

    플랫폼 추가 및 수정

--- Term Project/platform.py
+++ Term Project/platform.py
@@ -7,14 +7,15 @@ class Platform:
 	def __init__(self, type, left, bottom):
 		self.left = left
 		self.bottom = bottom
-		self.width, self.height, fn = INFO[type]
+		self.width, self.height = 16, 16
 		self.image = gfw.image.load(game_object.resBM('block.png'))
+		self.type = type
 
 	def update(self):
 		pass
 
 	def draw(self):
-		self.image.draw_to_origin(self.left, self.bottom, self.width, self.height)
+		self.image.clip_draw(self.type * 16, 16, 16, 16, 0, 0)
 
 	def get_bb(self):
 		return self.left, self.bottom, self.left + self.width, self.bottom + self.height
