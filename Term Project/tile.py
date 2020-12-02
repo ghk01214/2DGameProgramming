bf91a439cc17ec26dbe178fb94b95135aab40bb0
ghk01214 <ghk01214@naver.com>
2020-12-03 05:54:33 +0900

    json 배경 타일셋 및 타일 모듈

---
+++ Term Project/tile.py
@@ -0,0 +1,93 @@
+from pico2d import *
+import gfw
+import json
+
+class Layer:
+	def __init__(self, dict):
+		self.__dict__.update(dict)
+
+class Tileset:
+	def __init__(self, dict):
+		self.__dict__.update(dict)
+		self.rows = math.ceil((self.tilecount + 25) / self.columns)
+		print('rows: ', self.rows)
+
+	def getRectForTile(self, tile):
+		x = (tile - 1) % self.columns
+		y = (tile - 1) // self.columns
+		l = x * (self.tilewidth + self.spacing) + self.margin
+		t = (self.rows - y - 1) * (self.tileheight + self.spacing) + self.margin
+
+		return l, t - 8, self.tilewidth, self.tileheight
+
+class Map:
+	def __init__(self, dict):
+		self.__dict__.update(dict)
+		self.layers = list(map(Layer, self.layers))
+		self.tilesets = list(map(Tileset, self.tilesets))
+
+class Background:
+	def __init__(self, json_fn, tile_fn):
+		with open(json_fn) as f:
+			self.map = Map(json.load(f))
+
+		self.image = gfw.image.load(tile_fn)
+		self.width = self.map.tilewidth * self.map.width
+		self.height = self.map.tileheight * self.map.height - 8
+		self.tileset = self.map.tilesets[0]
+		self.layer = self.map.layers[0]
+		self.wraps = True
+		self.speed_x, self.speed_y = 0, 0
+		self.scroll_x, self.scroll_y = 0, 0
+
+	def get_boundary(self):
+		return 0, 0, self.width, self.height
+
+	def update(self):
+		self.scroll_x += self.speed_x * gfw.delta_time
+		self.scroll_y += self.speed_y * gfw.delta_time
+
+	def draw(self):
+		sx, sy = round(self.scroll_x), round(self.scroll_y)
+
+		if self.wraps:
+			sx %= self.width
+
+			if sx < 0:
+				sx += self.width
+
+			sy %= self.height
+
+			if sy <0:
+				sy += self.height
+
+		cw, ch = get_canvas_width(), get_canvas_height()
+
+		tile_x = sx // self.map.tilewidth
+		tile_y = sy // self.map.tileheight
+		beg_x = -(sx % self.map.tilewidth)
+		beg_y = -(sy % self.map.tileheight)
+
+		db = beg_y
+		ty = tile_y
+
+		while ty < self.layer.height and db < ch:
+			if ty >= 0:
+				dl = beg_x
+				dr = beg_x + self.map.tilewidth
+				tx = tile_x
+				ti = (self.map.height - ty - 1) * self.map.width + tx
+
+				while tx < self.layer.width and dl < cw:
+					tile = self.layer.data[ti]
+					rect = self.tileset.getRectForTile(tile)
+					self.image.clip_draw_to_origin(*rect, dl, db)
+					dl += self.map.tilewidth
+					ti += 1
+					tx += 1
+
+			db += self.map.tileheight
+			ty += 1
+
+			if self.wraps and ty >= self.layer.height:
+				ty -= self.layer.height
