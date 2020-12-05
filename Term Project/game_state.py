c04b76ba10b50ae4590c76edbcc9cdd7f436c5ca
ghk01214 <ghk01214@naver.com>
2020-12-06 00:24:22 +0900

    세이브 앤 로드

--- Term Project/game_state.py
+++ Term Project/game_state.py
@@ -7,6 +7,9 @@ from platform import Platform
 from tile import Tile
 from collision import check_collision
 import stage_gen
+import pickle
+
+FILENAME = 'save.data'
 
 def enter():
 	gfw.world.init(['tile', 'platform', 'player', 'bullet'])
@@ -15,12 +18,14 @@ def enter():
 	tile = Tile('res/stage_1.json', 'res/bitmap/tileset.png')
 	gfw.world.add(gfw.layer.tile, tile)
 
-	global player
+	global background, tile, player, stage_num
 	player = Player()
 	player.background = tile
 	gfw.world.add(gfw.layer.player, player)
 
 	stage_gen.load(resBM('../stage_1.txt'))
+	save()
+
 def exit():
 	pass
 
@@ -40,6 +45,10 @@ def handle_event(e):
 			gfw.pop()
 		elif e.key == SDLK_q:
 			gfw.quit()
+		elif e.key == SDLK_r:
+			load()
+		elif e.key == SDLK_s:
+			save()
 
 	if player.handle_event(e):
 		return
@@ -49,6 +58,31 @@ def pause():
 
 def resume():
 	pass
+
+def save():
+	global background, tile, player, stage_num
+
+	with open(FILENAME, "wb") as file:
+		pickle.dump(stage_num, file)
+		pickle.dump(background.pos, file)
+		pickle.dump(tile.scroll_x, file)		
+		pickle.dump(player.pos, file)
+
+def load():
+	global background, tile, player, stage_num
+
+	try:
+		with open(FILENAME, "rb") as file:
+			stage_num = pickle.load(file)
+			background.pos = pickle.load(file)
+			tile.scroll_x = pickle.load(file)
+			player.pos = pickle.load(file)
+
+		stage_gen.remove()
+	except:
+		print("No save file")
+
+	stage_gen.load(resBM('../stage_%d.txt' % stage_num))
 
 if __name__ == '__main__':
 	gfw.run_main()
